dd848104a3cf6a89d50cfb2f0e0983f562d1b443
mariaguzovskaya <mariaguzovskaaa@gmail.com>
2025-03-29 18:01:40 +0300

    homework 5

---
+++ homework4.js
@@ -0,0 +1,27 @@
+// Задание 1:
+let fruits = ["Яблоки", "Груша", "Апельсин"];
+
+// добавляем новое значение в "копию"
+let shoppingCart = fruits;
+shoppingCart.push("Банан");
+
+// что в fruits?
+alert( fruits.length ); // ? - выведет 4
+
+
+// Задание 2:
+let  styles = ["Джаз", "Блюз"]
+styles.push("Рок-н-ролл")
+styles[Math.floor((styles.length - 1) / 2)] = "Классика"; // Новый метод
+alert(styles.shift())
+styles.unshift("Рэп","Рэгги")
+alert(styles)
+
+// Задание 3:
+
+
+// Задание 4:
+
+
+// Задание 5:
+
